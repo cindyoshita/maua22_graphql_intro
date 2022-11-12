5faf8ce9228c229d46dee0399ed4a0f3135b9b52
cindyoshita <79462846+cindyoshita@users.noreply.github.com>
2022-11-12 08:48:05 -0300

    12/11/22
    - terminado apostila 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
 import { createServer } from "@graphql-yoga/node"
 
+const livros = [
+    {
+        id: '1',
+        titulo: 'Effective Java',
+        genero: "Técnico",
+        edicao: 3,
+        preco: 39.99
+    },
+    {
+        id: '2',
+        titulo: "Concrete Mathematics",
+        genero: "Técnico",
+        edicao: 1,
+        preco: 89.99
+    }
+];
+
+const usuarios = [{
+    id: '100',
+    nome: 'Jose',
+    livros: [{
+        id: '1',
+        titulo: 'Effective Java',
+        genero: "Técnico",
+        edicao: 3,
+        preco: 39.99
+    },
+    {
+        id: '2',
+        titulo: "Concrete Mathematics",
+        genero: "Técnico",
+        edicao: 1,
+        preco: 89.99
+    }
+    ]
+}, {
+    id: '101',
+    nome: 'Maria',
+    livros: [{
+        id: '5',
+        titulo: 'Programming Challenges',
+        genero: "Técnico",
+        edicao: 1,
+        preco: 39.99
+    }]
+}]
+
+
 const typeDefs = `
     type Query {
+        usuarios: [Usuario!]!
+        livros (precoMaximo: Float!): [Livro!]!
         adicionar(numeros:[Float!]):Float!
         notas: [Int!]!
         bemVindo(nome: String): String!
@@ -13,25 +63,42 @@ const typeDefs = `
         genero: String!
         edicao: Int
         preco: Float
-    }
+    },
+    type Usuario{
+        id: ID!,
+        nome: String!,
+        idade: Int!,
+        livros: [Livro!]
+        }
+       
 `;
 
 const resolvers = {
     Query: {
-        adicionar(parent, args, ctx, info){
-            return args.numeros.length === 0? 0 :
-            args.numeros.reduce((ac,atual) => {
-                return ac + atual;
-            })
+        usuarios() {
+            return usuarios;
+            },
+
+        livros(parent, args, ctx, info) {
+            return livros.filter((l) => {
+                return l.preco <= args.precoMaximo
+            });
+        },
+
+        adicionar(parent, args, ctx, info) {
+            return args.numeros.length === 0 ? 0 :
+                args.numeros.reduce((ac, atual) => {
+                    return ac + atual;
+                })
         },
 
         notas(parent, args, ctx, info) {
-            return[10, 2, 7, 7, 8]
+            return [10, 2, 7, 7, 8]
         },
 
         bemVindo(parent, args, ctx, info) {
             return `
-                Bem vindo ${args.nome ? args.nome: 'visitante'}
+                Bem vindo ${args.nome ? args.nome : 'visitante'}
                 `;
         },
 
